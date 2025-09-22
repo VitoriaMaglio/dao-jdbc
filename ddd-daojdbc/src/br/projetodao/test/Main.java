e02221aae067a10de224c8220bbd443f2932f1a5
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-09-22 14:01:22 -0300

    Adicionando a classe Seller no projeto.

--- ddd-daojdbc/src/br/projetodao/test/Main.java
+++ ddd-daojdbc/src/br/projetodao/test/Main.java
@@ -1,6 +1,9 @@
 package br.projetodao.test;
 
 import br.projetodao.model.Department;
+import br.projetodao.model.Seller;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         System.out.println(obj);
-
+        Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.0, obj);
+        System.out.println(seller);
 
 }}
